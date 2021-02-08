692cb4ac23ce66042cae59405b474eb1407ddb2b
lennoxcoder <76738750+lennoxcoder@users.noreply.github.com>
2021-02-08 11:25:45 -0300

    Add files via upload
    EJS, Express, MongoDB

---
+++ Agenda/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const route = express.Router();
+const controller = require('./src/controllers/controller');
+
+route.get('/', controller.index);
+route.get('/login', controller.login);
+route.post('/enter', controller.enter);
+route.post('/register', controller.register);
+route.get('/logout', controller.logout);
+route.get('/contacts', controller.contacts)
+route.get('/contacts/create', controller.createContact);
+route.post('/contact/register', controller.contactRegister);
+route.get('/contact/delete/:id', controller.deleteContact);
+
+
+module.exports = route;
